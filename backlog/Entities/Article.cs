c06e93c142a2c68fd9c24fa5df2fcc4d0af6a3cc
Fabian <schenkfab@users.noreply.github.com>
2020-05-09 21:16:04 +0200

    Add Author to the Article

--- backlog/Entities/Article.cs
+++ backlog/Entities/Article.cs
@@ -7,6 +7,7 @@ namespace backlog.Entities
         public String Name { get; set; }
         public String Picture { get; set; }
         public String Description { get; set; }
+        public String Author { get; set; }
         public DateTime Date { get; set; }
         public virtual Feed Feed { get; set; }
     }
