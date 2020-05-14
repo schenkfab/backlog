c910aae52192b6d2fe941ffa8fdf944885e2a444
Fabian <schenkfab@users.noreply.github.com>
2020-05-14 18:17:47 +0200

    default image if a blog doesnt have one

--- backlogweb/src/components/kanban/Card.vue
+++ backlogweb/src/components/kanban/Card.vue
@@ -2,7 +2,8 @@
     <li class="p-2 mb-3 bg-white shadow rounded-lg cursor-move" style="min-width: 300px;">
         <div class="flex justify-between items-center">
           <div class="flex items-center">
-            <img class="w-10 h-10 rounded-full" :src="data.picture" :alt="data.name">
+            <img class="w-10 h-10 rounded-full" v-if="data.picture" :src="data.picture" :alt="data.name">
+            <img class="w-10 h-10 rounded-full" v-if="!data.picture" src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Generic_Feed-icon.svg/1024px-Generic_Feed-icon.svg.png" :alt="data.name">
             <p class="ml-2 text-gray-700 font-sans tracking-wide text-sm">{{data.name}}</p>
           </div>
           <div class="flex">
